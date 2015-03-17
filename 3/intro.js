695d76c7823a55bdd059f746f1e9274bf46a634d
liuy147 <liuy147@newschool.edu>
2015-03-16 21:02:24 -0400

    3
    3

---
+++ 3/intro.js
@@ -0,0 +1,12 @@
+
+var name = prompt("What is your name?");
+document.getElementById("xholder").innerHTML = name;
+
+var color= prompt("What is your favorite color?");
+document.getElementById("xholder2").innerHTML = color;
+
+var birthday= prompt("What is your birthday?");
+document.getElementById("xholder3").innerHTML = birthday;
+
+document.body.style.color = color;
+
